ef327144ce39ad17dc1980dc9bd11e9801d3bf35
Moran <thaler.moran@gmail.com>
2022-06-14 19:00:16 +0200

    added the block code from BEM website, css section

---
+++ README.md
@@ -0,0 +1,7 @@
+## Sass loves React 💅
+
+## Explain Sass in baby steps (1px step;)
+
+### check all the commits to get the steps & clone your checkpoint to get started
+
+#### ENJOY 🙆‍♀️
--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>Explain Sass 💅</h1>
+      {/* logo block */}
+      <div className="log logo_theme_islands">
+        <img src="URL" alt="logo" className="logo__img" />
+      </div>
+
+      <div className="user user_theme_islands">
+        <img src="URL" alt="user-logo" className="user__img" />
+      </div>
     </div>
   );
 }
