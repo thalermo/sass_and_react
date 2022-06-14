fa7d36bacc9e6b86571ffc1f221fd1ff41ec85c2
Moran <thaler.moran@gmail.com>
2022-06-14 19:13:47 +0200

    fixed the script in json frim scss to sass and tested the compiling for the main.scss to main.css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './main.css';
 function App() {
   return (
     <div className="App">
-      <h1>Explain Sass 💅</h1>
+      <h1 className="h1">Explain Sass 💅</h1>
+
       {/* logo block */}
       <div className="log logo_theme_islands">
         <img src="URL" alt="logo" className="logo__img" />
       </div>
 
+      {/* user block  */}
       <div className="user user_theme_islands">
         <img src="URL" alt="user-logo" className="user__img" />
       </div>
